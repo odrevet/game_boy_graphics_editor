4157d7d183fedaae1cae32c88bbaaa3793c5d48d
odrevet <odrevet@gmail.com>
2022-03-01 00:13:12 +0100

    CustomPainter WIP

--- lib/widgets/tile_grid.dart
+++ lib/widgets/tile_grid.dart
@@ -1,35 +1,53 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class TileGrid extends SingleChildRenderObjectWidget {
+class TileGrid extends StatefulWidget {
   final List<Color> pixels;
 
   const TileGrid({Key? key, required this.pixels}) : super(key: key);
 
   @override
-  PixelRenderBox createRenderObject(BuildContext context) {
-    return PixelRenderBox(pixels: pixels);
+  _TileGridState createState() => _TileGridState();
+}
+
+class _TileGridState extends State<TileGrid> {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.0,
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return CustomPaint(
+            size: Size(
+              constraints.maxWidth,
+              constraints.maxHeight,
+            ),
+            painter:
+                PixelPainter(pixels: widget.pixels, constraints: constraints),
+          );
+        },
+      ),
+    );
   }
 }
 
-class PixelRenderBox extends RenderBox {
+class PixelPainter extends CustomPainter {
   final Paint painter = Paint();
   final int crossAxisCount = 8;
-  late double pixelSize;
+  late double pixelSize = 10;
   final List<Color> pixels;
+  final BoxConstraints constraints;
 
-  PixelRenderBox({required this.pixels});
+  PixelPainter({required this.pixels, required this.constraints});
 
   @override
-  void paint(PaintingContext context, Offset offset) {
+  void paint(Canvas canvas, Size size) {
+    pixelSize = constraints.maxWidth / 8;
     pixels.asMap().forEach((index, pixel) {
       painter.color = pixel;
-      context.canvas.drawRect(
+      canvas.drawRect(
           Rect.fromLTWH(
-            offset.dx +
-                (index % crossAxisCount).floor().toDouble() * pixelSize,
-            offset.dy +
-                (index / crossAxisCount).floor().toDouble() * pixelSize,
+            (index % crossAxisCount).floor().toDouble() * pixelSize,
+            (index / crossAxisCount).floor().toDouble() * pixelSize,
             pixelSize,
             pixelSize,
           ),
@@ -38,11 +56,5 @@ class PixelRenderBox extends RenderBox {
   }
 
   @override
-  void performLayout() {
-    size = Size(
-      constraints.constrainWidth(100),
-      constraints.constrainHeight(100),
-    );
-    pixelSize = size.width / crossAxisCount;
-  }
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
