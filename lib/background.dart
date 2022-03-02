c29911a1bf018e5385f60476780c194d4a342167
odrevet <odrevet@gmail.com>
2022-03-02 23:05:27 +0100

    Background extends Graphics

--- lib/background.dart
+++ lib/background.dart
@@ -1,20 +1,27 @@
+import 'package:gbdk_graphic_editor/graphics.dart';
+
 import 'convert.dart';
 
-class Background {
-  String name;
+class Background extends Graphics {
   int height;
   int width;
-  List<int> data = [];
 
-  Background({this.height = 0, this.width = 0, this.name = "", int fill = 0}) {
-    data = List.filled(height * width, fill, growable: true);
-  }
+  Background({this.height = 0, this.width = 0, name = "", int fill = 0})
+      : super(
+            name: name,
+            data: List<int>.filled(height * width, fill, growable: true));
 
+  @override
   String toSource() {
     return """
     #define ${name}Width $width
     #define ${name}Height $height
     #define ${name}Bank 0
     unsigned char $name[] =\n{${data.map((e) => decimalToHex(e)).join(",")}\n};""";
+  }
+
+  @override
+  void fromSource(String source) {
+    // TODO: implement fromSource
   }
 }
