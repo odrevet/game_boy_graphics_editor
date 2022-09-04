2aecb39141cda31b2bc52d7097ea6490db8c83b4
odrevet <odrevet@gmail.com>
2022-09-04 21:34:33 +0200

    flood mode OK

--- lib/models/meta_tile.dart
+++ lib/models/meta_tile.dart
@@ -31,7 +31,27 @@ class MetaTile {
   void setRow(int tileIndex, int rowIndex, List<int> row) {
     for (int dotIndex = 0; dotIndex < width; dotIndex++) {
       setPixel(dotIndex, rowIndex, tileIndex, row[dotIndex]);
-      ;
     }
   }
+
+  flood(int metaTileIndex, int intensity, int rowIndex, int colIndex, int targetColor) {
+    if (getPixel(rowIndex, colIndex, metaTileIndex) == targetColor) {
+      setPixel(rowIndex, colIndex, metaTileIndex, intensity);
+      if (inbound(rowIndex, colIndex - 1)) {
+        flood(metaTileIndex, intensity, rowIndex, colIndex - 1, targetColor);
+      }
+      if (inbound(rowIndex, colIndex + 1)) {
+        flood(metaTileIndex, intensity, rowIndex, colIndex + 1, targetColor);
+      }
+      if (inbound(rowIndex - 1, colIndex)) {
+        flood(metaTileIndex, intensity, rowIndex - 1, colIndex, targetColor);
+      }
+      if (inbound(rowIndex + 1, colIndex)) {
+        flood(metaTileIndex, intensity, rowIndex + 1, colIndex, targetColor);
+      }
+    }
+  }
+
+  inbound(int rowIndex, int colIndex) =>
+      rowIndex >= 0 && rowIndex < height && colIndex >= 0 && colIndex < width;
 }
