cfe695161e81f5fa20ac044f9ba932b1dbed6685
odrevet <odrevet@gmail.com>
2023-12-02 06:56:07 +0100

    display background using two_dimensional_scrollables

--- lib/widgets/background/background_grid.dart
+++ lib/widgets/background/background_grid.dart
@@ -1,18 +1,15 @@
-import 'dart:math' as math;
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_boy_graphics_editor/cubits/background_cubit.dart';
 import 'package:game_boy_graphics_editor/models/graphics/background.dart';
+import 'package:two_dimensional_scrollables/two_dimensional_scrollables.dart';
 
 import '../../cubits/meta_tile_cubit.dart';
 import '../../models/graphics/meta_tile.dart';
 import '../tiles/meta_tile_display.dart';
 
-class BackgroundGrid extends StatelessWidget {
+class BackgroundGrid extends StatefulWidget {
   final Background background;
   final MetaTile metaTile;
   final Function? onTap;
@@ -29,25 +26,35 @@ class BackgroundGrid extends StatelessWidget {
   });
 
   @override
+  State<BackgroundGrid> createState() => _BackgroundGridState();
+}
+
+class _BackgroundGridState extends State<BackgroundGrid> {
+  late final ScrollController _verticalController = ScrollController();
+  late final ScrollController _horizontalController = ScrollController();
+
+  int currentRow = 0;
+  int currentCol = 0;
+  final int cellSize = 50;
+
+  @override
   Widget build(BuildContext context) {
-    return TwoDimensionalGridView(
-      diagonalDragBehavior: DiagonalDragBehavior.free,
-      delegate: TwoDimensionalChildBuilderDelegate(
-          maxXIndex: background.width - 1,
-          maxYIndex: background.height - 1,
-          builder: (BuildContext context, ChildVicinity vicinity) {
-            return SizedBox(
-              height: 40,
-              width: 40,
-              child: Center(child: _buildCell(context, vicinity)),
-            );
-          }),
+    return TableView.builder(
+      verticalDetails:
+          ScrollableDetails.vertical(controller: _verticalController),
+      horizontalDetails:
+          ScrollableDetails.horizontal(controller: _horizontalController),
+      cellBuilder: _buildCell,
+      columnCount: widget.background.width,
+      columnBuilder: _buildColumnSpan,
+      rowCount: widget.background.height,
+      rowBuilder: _buildRowSpan,
     );
   }
 
   Widget _buildCell(BuildContext context, vicinity) {
-    int index = vicinity.yIndex * background.width + vicinity.xIndex;
-    if (background.data[index] >=
+    int index = vicinity.yIndex * widget.background.width + vicinity.xIndex;
+    if (widget.background.data[index] >=
         (context.read<MetaTileCubit>().state.data.length ~/
                 (context.read<MetaTileCubit>().state.height *
                     context.read<MetaTileCubit>().state.width)) +
@@ -55,168 +62,56 @@ class BackgroundGrid extends StatelessWidget {
       return Container(
         alignment: Alignment.center,
         child: Text(
-          "${background.data[index]} !",
+          "${widget.background.data[index]}",
           style: const TextStyle(color: Colors.red),
           textAlign: TextAlign.center,
-          textDirection: TextDirection.ltr,
         ),
       );
     } else {
       return MetaTileDisplay(
-        tileData: metaTile.getMetaTile(background.data[index] -
+        tileData: widget.metaTile.getMetaTile(widget.background.data[index] -
             context.read<BackgroundCubit>().state.origin),
       );
     }
   }
-}
 
-class TwoDimensionalGridView extends TwoDimensionalScrollView {
-  const TwoDimensionalGridView({
-    super.key,
-    super.primary,
-    super.mainAxis = Axis.vertical,
-    super.verticalDetails = const ScrollableDetails.vertical(),
-    super.horizontalDetails = const ScrollableDetails.horizontal(),
-    required TwoDimensionalChildBuilderDelegate delegate,
-    super.cacheExtent,
-    super.diagonalDragBehavior = DiagonalDragBehavior.none,
-    super.dragStartBehavior = DragStartBehavior.start,
-    super.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
-    super.clipBehavior = Clip.hardEdge,
-  }) : super(delegate: delegate);
-
-  @override
-  Widget buildViewport(
-    BuildContext context,
-    ViewportOffset verticalOffset,
-    ViewportOffset horizontalOffset,
-  ) {
-    return TwoDimensionalGridViewport(
-      horizontalOffset: horizontalOffset,
-      horizontalAxisDirection: horizontalDetails.direction,
-      verticalOffset: verticalOffset,
-      verticalAxisDirection: verticalDetails.direction,
-      mainAxis: mainAxis,
-      delegate: delegate as TwoDimensionalChildBuilderDelegate,
-      cacheExtent: cacheExtent,
-      clipBehavior: clipBehavior,
+  TableSpan _buildColumnSpan(int index) {
+    const TableSpanDecoration decoration = TableSpanDecoration(
+      border: TableSpanBorder(
+        trailing: BorderSide(),
+      ),
     );
-  }
-}
 
-class TwoDimensionalGridViewport extends TwoDimensionalViewport {
-  const TwoDimensionalGridViewport({
-    super.key,
-    required super.verticalOffset,
-    required super.verticalAxisDirection,
-    required super.horizontalOffset,
-    required super.horizontalAxisDirection,
-    required TwoDimensionalChildBuilderDelegate super.delegate,
-    required super.mainAxis,
-    super.cacheExtent,
-    super.clipBehavior = Clip.hardEdge,
-  });
-
-  @override
-  RenderTwoDimensionalViewport createRenderObject(BuildContext context) {
-    return RenderTwoDimensionalGridViewport(
-      horizontalOffset: horizontalOffset,
-      horizontalAxisDirection: horizontalAxisDirection,
-      verticalOffset: verticalOffset,
-      verticalAxisDirection: verticalAxisDirection,
-      mainAxis: mainAxis,
-      delegate: delegate as TwoDimensionalChildBuilderDelegate,
-      childManager: context as TwoDimensionalChildManager,
-      cacheExtent: cacheExtent,
-      clipBehavior: clipBehavior,
-    );
+    return TableSpan(
+        foregroundDecoration: decoration,
+        extent: const FixedTableSpanExtent(40),
+        onEnter: (_) => setState(() {
+              currentCol = index;
+            }));
   }
 
-  @override
-  void updateRenderObject(
-    BuildContext context,
-    RenderTwoDimensionalGridViewport renderObject,
-  ) {
-    renderObject
-      ..horizontalOffset = horizontalOffset
-      ..horizontalAxisDirection = horizontalAxisDirection
-      ..verticalOffset = verticalOffset
-      ..verticalAxisDirection = verticalAxisDirection
-      ..mainAxis = mainAxis
-      ..delegate = delegate
-      ..cacheExtent = cacheExtent
-      ..clipBehavior = clipBehavior;
-  }
-}
-
-class RenderTwoDimensionalGridViewport extends RenderTwoDimensionalViewport {
-  RenderTwoDimensionalGridViewport({
-    required super.horizontalOffset,
-    required super.horizontalAxisDirection,
-    required super.verticalOffset,
-    required super.verticalAxisDirection,
-    required TwoDimensionalChildBuilderDelegate delegate,
-    required super.mainAxis,
-    required super.childManager,
-    super.cacheExtent,
-    super.clipBehavior = Clip.hardEdge,
-  }) : super(delegate: delegate);
-
-  @override
-  void layoutChildSequence() {
-    final double horizontalPixels = horizontalOffset.pixels;
-    final double verticalPixels = verticalOffset.pixels;
-    final double viewportWidth = viewportDimension.width + cacheExtent;
-    final double viewportHeight = viewportDimension.height + cacheExtent;
-    final TwoDimensionalChildBuilderDelegate builderDelegate =
-        delegate as TwoDimensionalChildBuilderDelegate;
-
-    final int maxRowIndex = builderDelegate.maxYIndex!;
-    final int maxColumnIndex = builderDelegate.maxXIndex!;
-
-    const cellSize = 40;
-
-    final int leadingColumn = math.max((horizontalPixels / cellSize).floor(), 0);
-    final int leadingRow = math.max((verticalPixels / cellSize).floor(), 0);
-    final int trailingColumn = math.min(
-      ((horizontalPixels + viewportWidth) / cellSize).ceil(),
-      maxColumnIndex,
-    );
-    final int trailingRow = math.min(
-      ((verticalPixels + viewportHeight) / cellSize).ceil(),
-      maxRowIndex,
+  TableSpan _buildRowSpan(int index) {
+    TableSpanDecoration decoration = TableSpanDecoration(
+      border: widget.showGrid
+          ? const TableSpanBorder(
+              trailing: BorderSide(
+                width: 10,
+              ),
+            )
+          : null,
     );
 
-
-    double xLayoutOffset = (leadingColumn * cellSize) - horizontalOffset.pixels;
-    for (int column = leadingColumn; column <= trailingColumn; column++) {
-      double yLayoutOffset = (leadingRow * cellSize) - verticalOffset.pixels;
-      for (int row = leadingRow; row <= trailingRow; row++) {
-        final ChildVicinity vicinity =
-            ChildVicinity(xIndex: column, yIndex: row);
-        final RenderBox child = buildOrObtainChildFor(vicinity)!;
-        child.layout(constraints.loosen());
-
-        // Subclasses only need to set the normalized layout offset. The super
-        // class adjusts for reversed axes.
-        parentDataOf(child).layoutOffset = Offset(xLayoutOffset, yLayoutOffset);
-        yLayoutOffset += cellSize;
-      }
-      xLayoutOffset += cellSize;
-    }
-
-    // Set the min and max scroll extents for each axis.
-    final double verticalExtent = cellSize * (maxRowIndex + 1);
-    verticalOffset.applyContentDimensions(
-      0.0,
-      clampDouble(
-          verticalExtent - viewportDimension.height, 0.0, double.infinity),
-    );
-    final double horizontalExtent = cellSize * (maxColumnIndex + 1);
-    horizontalOffset.applyContentDimensions(
-      0.0,
-      clampDouble(
-          horizontalExtent - viewportDimension.width, 0.0, double.infinity),
+    return TableSpan(
+      backgroundDecoration: decoration,
+      extent: const FixedTableSpanExtent(40),
+      recognizerFactories: <Type, GestureRecognizerFactory>{
+        TapGestureRecognizer:
+            GestureRecognizerFactoryWithHandlers<TapGestureRecognizer>(
+          () => TapGestureRecognizer(),
+          (TapGestureRecognizer t) =>
+              t.onTap = () => print('$index:$currentCol'),
+        ),
+      },
     );
   }
 }
