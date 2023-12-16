2619e83674605417003788ea8e14d613bc6afc58
odrevet <odrevet@gmail.com>
2023-12-16 16:06:46 +0100

    display tile index + tile origin

--- lib/widgets/tiles/meta_tile_list_view.dart
+++ lib/widgets/tiles/meta_tile_list_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:game_boy_graphics_editor/cubits/background_cubit.dart';
 import 'package:game_boy_graphics_editor/cubits/meta_tile_cubit.dart';
 
+import '../../cubits/app_state_cubit.dart';
 import '../../models/sourceConverters/source_converter.dart';
 import 'meta_tile_display.dart';
 
@@ -19,11 +21,16 @@ class MetaTileListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int tileOrigin = context.read<BackgroundCubit>().state.tileOrigin;
     var metaTile = context.read<MetaTileCubit>().state;
     return ListView.builder(
       shrinkWrap: true,
       itemCount: context.read<MetaTileCubit>().count(),
       itemBuilder: (context, index) {
+        String title = "${index.toString()} ${decimalToHex(index, prefix: true)}";
+        if(tileOrigin > 0 && !context.read<AppStateCubit>().state.tileMode){
+          title += "\n${(index + tileOrigin).toString()} ${decimalToHex(index + tileOrigin, prefix: true)}";
+        }
         return ListTile(
           leading: SizedBox(
             width: 40 * (metaTile.width / metaTile.height),
@@ -34,7 +41,7 @@ class MetaTileListView extends StatelessWidget {
             ),
           ),
           title: Text(
-              "#${index.toString()} ${decimalToHex(index, prefix: true)}",
+              title,
               style: selectedTile == index
                   ? const TextStyle(
                       color: Colors.blue, fontWeight: FontWeight.bold)
