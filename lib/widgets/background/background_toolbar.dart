5d4e41724ff8fb15451741fbb0c6dae9ede6afc3
odrevet <odrevet@gmail.com>
2023-12-31 16:40:34 +0100

    undo / redo Background

--- lib/widgets/background/background_toolbar.dart
+++ lib/widgets/background/background_toolbar.dart
@@ -2,27 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../cubits/app_state_cubit.dart';
-
+import '../../cubits/background_cubit.dart';
+import '../../models/graphics/background.dart';
 
 class BackgroundToolbar extends StatelessWidget {
   const BackgroundToolbar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const VerticalDivider(),
-        IconButton(
-            onPressed: context.read<AppStateCubit>().state.zoomBackground >= 0.4
-                ? context.read<AppStateCubit>().decreaseZoomBackground
-                : null,
-            icon: const Icon(Icons.zoom_out)),
-        IconButton(
-            onPressed: context.read<AppStateCubit>().state.zoomBackground <= 0.8
-                ? context.read<AppStateCubit>().increaseZoomBackground
-                : null,
-            icon: const Icon(Icons.zoom_in)),
-      ],
-    );
+    return BlocBuilder<BackgroundCubit, Background>(
+        builder: (context, appState) => Row(
+              children: [
+                const VerticalDivider(),
+                IconButton(
+                    onPressed: context.read<BackgroundCubit>().canUndo
+                        ? context.read<BackgroundCubit>().undo
+                        : null,
+                    icon: const Icon(Icons.undo)),
+                IconButton(
+                    onPressed: context.read<BackgroundCubit>().canRedo
+                        ? context.read<BackgroundCubit>().redo
+                        : null,
+                    icon: const Icon(Icons.redo)),
+                const VerticalDivider(),
+                IconButton(
+                    onPressed: context
+                                .read<AppStateCubit>()
+                                .state
+                                .zoomBackground >=
+                            0.4
+                        ? context.read<AppStateCubit>().decreaseZoomBackground
+                        : null,
+                    icon: const Icon(Icons.zoom_out)),
+                IconButton(
+                    onPressed: context
+                                .read<AppStateCubit>()
+                                .state
+                                .zoomBackground <=
+                            0.8
+                        ? context.read<AppStateCubit>().increaseZoomBackground
+                        : null,
+                    icon: const Icon(Icons.zoom_in)),
+              ],
+            ));
   }
 }
