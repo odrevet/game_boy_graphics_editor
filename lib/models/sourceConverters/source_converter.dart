7f4ea8cec60b854f82a2a0e3797913421003266f
odrevet <odrevet@gmail.com>
2025-09-03 17:34:20 +0200

    remove obsolete source converters functions

--- lib/models/sourceConverters/source_converter.dart
+++ lib/models/sourceConverters/source_converter.dart
@@ -19,31 +19,6 @@ abstract class SourceConverter {
 
   String toSource(Graphics graphics, String name);
 
-  String formatSource(String source) {
-    LineSplitter ls = const LineSplitter();
-    List<String> lines = ls.convert(source);
-    return lines.join();
-  }
-
-  List<Graphics> readGraphicsFromSource(String source) {
-    var graphics = <Graphics>[];
-
-    RegExp regExp = RegExp(
-      r"(?:unsigned\s+char|uint8_t|UINT8)\s+(\w+)\[(?:\d+)?\]\s*=\s*\{(.*?)};",
-    );
-    for (Match match in regExp.allMatches(source)) {
-      // remove trailing comma if any
-      String matchedValues = match.group(2)!.replaceFirst(RegExp(r',\s*$'), '');
-
-      List<int> values = List<int>.from(
-        matchedValues.split(',').map((value) => int.parse(value)).toList(),
-      );
-      graphics.add(Graphics(name: match.group(1)!, data: values));
-    }
-
-    return graphics;
-  }
-
   Map<String, int> readDefinesFromSource(String source) {
     Map<String, int> defines = {};
     RegExp regExp = RegExp(r"#define (\w+) (\d+)");
