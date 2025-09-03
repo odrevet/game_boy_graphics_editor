aa7bc70e5051bc7e9ffd1344950993b655e72eea
odrevet <odrevet@gmail.com>
2025-09-03 17:31:30 +0200

    remove obsolet/unused functions

--- lib/models/import_callbacks.dart
+++ lib/models/import_callbacks.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:game_boy_graphics_editor/models/sourceConverters/converter_utils.dart';
@@ -9,12 +8,7 @@ import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as p;
 
 import '../cubits/app_state_cubit.dart';
-import '../cubits/background_cubit.dart';
-import '../cubits/meta_tile_cubit.dart';
-import '../models/graphics/background.dart';
 import '../models/graphics/graphics.dart';
-import '../models/sourceConverters/gbdk_background_converter.dart';
-import '../models/sourceConverters/gbdk_tile_converter.dart';
 import 'file_picker_utils.dart';
 
 Future<List<Graphics>?> onImportHttp(
@@ -50,74 +44,7 @@ Future<List<Graphics>?> onImportHttp(
 
     return graphicsElements;
   }
-
-  /*  bool tile = parse == 'Tile';
-  Uri uriObject = Uri.parse(url);
-
-  if (type == 'Auto') {
-    type = resolveType(uriObject.path);
-  }
-
-  if (type == 'Binary') {
-    http.readBytes(uriObject).then((content) {
-      loadBin(content, tile, transpose, context);
-    });
-  } else {
-    http.read(uriObject).then((source) {
-      if (tile) {
-        _setTilesFromSource(source, context);
-      } else {
-        _setBackgroundFromSource(source, context);
-      }
-    });
-  }*/
 }
-
-/*onImport(
-  BuildContext context,
-  String type,
-  bool transpose,
-  String compression,
-) {
-
-  selectFile(['*']).then((result) {
-    if (result != null) {
-      if (type == 'Auto') {
-        type = resolveType(result.files.single.name);
-      }
-
-      // WIP
-//      if (type == 'Binary') {
-//        if (compression != 'none') {
-//          String inputPath = result.files.single.path!;
-//          List<int> content = _decompress(inputPath, compression, context);
-//          if (content.isNotEmpty) {
-//            loadBin(content, tile, transpose, context);
-//          }
-//        } else {
-//          readBin(result).then((List<int> content) {
-//            loadBin(content, tile, transpose, context);
-//          });
-//        }
-//      } else {
-//        readString(result).then((String source) {
-//          if (tile) {
-//            _setTilesFromSource(source, context);
-//          } else {
-//            _setBackgroundFromSource(source, context);
-//          }
-//        });
-//      }
-
-      readString(result).then((String source) {
-        source = GBDKTileConverter().formatSource(source);
-        List<Graphics> graphicsElements = GBDKTileConverter()
-            .readGraphicsFromSource(source);
-        context.read<GraphicsCubit>().addGraphics(graphicsElements);
-      });
-    }
-  });
-}*/
 
 Future<List<Graphics>?> onImport(
   BuildContext context,
@@ -147,14 +74,6 @@ Future<List<Graphics>?> onImport(
     return [graphics];
   } else {
     final source = await readString(result);
-
-    // using source converter (regexp based)
-    //final formattedSource = GBDKTileConverter().formatSource(source);
-    //final graphicsElements = GBDKTileConverter().readGraphicsFromSource(
-    //  formattedSource,
-    //);
-
-    // using source parser (petitparser based)
     final parser = SourceParser();
     final graphicsElements = parser.parseAllArrays(source);
 
@@ -168,67 +87,6 @@ String resolveType(String path) {
     return 'Source code';
   } else {
     return 'Binary';
-  }
-}
-
-void loadBin(
-  List<int> content,
-  bool tile,
-  bool transpose,
-  BuildContext context,
-) {
-  if (tile) {
-    List<int> data = GBDKTileConverter().combine(content);
-    data = GBDKTileConverter().reorderFromSourceToCanvas(
-      data,
-      context.read<MetaTileCubit>().state.width,
-      context.read<MetaTileCubit>().state.height,
-    );
-    context.read<MetaTileCubit>().setData(data);
-  } else {
-    List<int> data = convertBytesToDecimals(content);
-    if (transpose) {
-      _setBackgroundFromBinTransposed(data, context);
-    } else {
-      _setBackgroundFromBin(data, context);
-    }
-  }
-}
-
-void _setTilesFromSource(String source, BuildContext context) {
-  source = GBDKTileConverter().formatSource(source);
-
-  Map<String, int> defines = GBDKTileConverter().readDefinesFromSource(source);
-  _setPropertiesFromDefines(defines, context);
-
-  var graphicsElements = GBDKTileConverter().readGraphicsFromSource(source);
-  if (graphicsElements.length > 1) {
-    _showGraphicsChooseDialog(context, graphicsElements, _setMetaTile);
-  } else if (graphicsElements.length == 1) {
-    _setMetaTile(graphicsElements.first, context);
-  }
-}
-
-void _setBackgroundFromSource(String source, BuildContext context) {
-  source = GBDKBackgroundConverter().formatSource(source);
-
-  Map<String, int> defines = GBDKBackgroundConverter().readDefinesFromSource(
-    source,
-  );
-  _setPropertiesFromDefines(defines, context);
-
-  var graphicsElements = GBDKBackgroundConverter().readGraphicsFromSource(
-    source,
-  );
-  if (graphicsElements.length > 1) {
-    _showGraphicsChooseDialog(
-      context,
-      graphicsElements,
-      _setBackgroundFromGraphics,
-    );
-  } else if (graphicsElements.length == 1) {
-    context.read<AppStateCubit>().setSelectedTileIndex(0);
-    _setBackgroundFromGraphics(graphicsElements.first, context);
   }
 }
 
@@ -255,94 +113,4 @@ List<int> _decompress(
   }
 
   return content;
-}
-
-bool _setMetaTile(Graphics Graphics, BuildContext context) {
-  bool hasLoaded = true;
-  try {
-    context.read<AppStateCubit>().setTileName(Graphics.name);
-    var data = GBDKTileConverter().combine(Graphics.data);
-    data = GBDKTileConverter().reorderFromSourceToCanvas(
-      data,
-      context.read<MetaTileCubit>().state.width,
-      context.read<MetaTileCubit>().state.height,
-    );
-    context.read<MetaTileCubit>().setData(data);
-  } catch (e) {
-    if (kDebugMode) {
-      print("ERROR $e");
-    }
-    hasLoaded = false;
-  }
-
-  if (hasLoaded) context.read<AppStateCubit>().setSelectedTileIndex(0);
-
-  return hasLoaded;
-}
-
-void _setPropertiesFromDefines(Map<String, int> defines, BuildContext context) {
-  defines.forEach((key, value) {
-    if (key.endsWith('TILE_ORIGIN')) {
-      context.read<BackgroundCubit>().setOrigin(value);
-    } else if (key.endsWith('WIDTH')) {
-      context.read<BackgroundCubit>().setWidth(value ~/ 8);
-    } else if (key.endsWith('HEIGHT')) {
-      context.read<BackgroundCubit>().setHeight(value ~/ 8);
-    }
-  });
-}
-
-bool _setBackgroundFromGraphics(Graphics Graphics, BuildContext context) {
-  Background background = GBDKBackgroundConverter().fromGraphics(Graphics);
-  context.read<BackgroundCubit>().setData(background.data);
-  return true;
-}
-
-_showGraphicsChooseDialog(
-  BuildContext context,
-  List<Graphics> graphicsElements,
-  Function onTap,
-) {
-  bool hasLoaded = false;
-  showDialog(
-    context: context,
-    barrierDismissible: false,
-    builder: (BuildContext context) => AlertDialog(
-      title: const Text('Graphic element selection'),
-      content: SizedBox(
-        height: 200.0,
-        width: 150.0,
-        child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: graphicsElements.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              onTap: () {
-                onTap(graphicsElements[index], context);
-                Navigator.pop(context);
-              },
-              title: Text(graphicsElements[index].name),
-            );
-          },
-        ),
-      ),
-    ),
-  );
-
-  return hasLoaded;
-}
-
-void _setBackgroundFromBin(List<int> raw, BuildContext context) {
-  Graphics graphics = Background(data: raw);
-  context.read<BackgroundCubit>().setData(graphics.data);
-  context.read<AppStateCubit>().setBackgroundName("data");
-}
-
-void _setBackgroundFromBinTransposed(List<int> raw, BuildContext context) {
-  raw = transpose(
-    raw,
-    context.read<BackgroundCubit>().state.height,
-    context.read<BackgroundCubit>().state.width,
-  );
-  _setBackgroundFromBin(raw, context);
 }
