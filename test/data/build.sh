da781e453f159648b0bc398aadc146d39ba9e0b6
odrevet <odrevet@gmail.com>
2025-09-05 05:51:00 +0200

    rebuild test data

--- test/data/build.sh
+++ test/data/build.sh
@@ -1,3 +1,4 @@
+png2asset numbers.png
 png2asset numbers.png -o number.bin -map -bin
 png2asset numbers.png -o "numbers_transposed.c" -transposed -map
 gbcompress number_map.bin number_map_compressed.bin
