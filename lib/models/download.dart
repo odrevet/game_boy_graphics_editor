964278435d0bcd95158acfa26923e70e8271cf18
odrevet <odrevet@gmail.com>
2025-09-07 07:51:33 +0200

    use web package instead of html package

--- lib/models/download.dart
+++ lib/models/download.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
+import 'package:web/web.dart';
 
 void download(String content, String downloadName) {
   final base64 = base64Encode(content.codeUnits);
-  final anchor = html.AnchorElement(
-    href: 'data:application/octet-stream;base64,$base64',
-  )..target = 'blank';
-  anchor.download = downloadName;
-  html.document.body?.append(anchor);
+  final anchor = HTMLAnchorElement()
+    ..href = 'data:application/octet-stream;base64,$base64'
+    ..target = '_blank'
+    ..download = downloadName;
+
+  document.body?.appendChild(anchor);
   anchor.click();
   anchor.remove();
 }
