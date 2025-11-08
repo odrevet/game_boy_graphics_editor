2e6e1156fa7a4a4466f812d68d6808839c262e02
odrevet <odrevet@gmail.com>
2025-11-08 10:52:39 +0100

    set application version to 2.1.0 in about dialog

--- lib/widgets/menu_bar.dart
+++ lib/widgets/menu_bar.dart
@@ -142,7 +142,7 @@ class ApplicationMenuBar extends StatelessWidget {
                   showAboutDialog(
                     context: context,
                     applicationName: 'GameBoy Graphics Editor',
-                    applicationVersion: '2.0.0',
+                    applicationVersion: '2.1.0',
                   );
                 },
                 child: const MenuAcceleratorLabel('&About'),
